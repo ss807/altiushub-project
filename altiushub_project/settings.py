f6d0b2116b3ede738b3440809b681a427a761858
Shubham Goyal <shubham807.sg@gmail.com>
2024-02-03 16:28:55 +0530

    ahead

--- altiushub_project/settings.py
+++ altiushub_project/settings.py
@@ -131,3 +131,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
